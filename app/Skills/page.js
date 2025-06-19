0ae12018014de9017d27e8fbfe1d8ebe24299c55
MatthewTsui2001 <matttsui2001@gmail.com>
2025-06-19 16:51:27 -0700

    made minor change to the roundness of the resume #12

--- app/Skills/page.js
+++ app/Skills/page.js
@@ -16,7 +16,7 @@ export default function Home() {
         <main class="mx-auto my-4 w-full bg-gradient-to-t from-gray-500 to-white">
           <h1 class="text-center text-5xl sm:text-6xl md:text-7xl my-4">Resume</h1>
           <div class="flex flex-col">
-            <iframe src="Target Resume.pdf" class="w-300 h-[600px] m-auto"></iframe>
+            <iframe src="Target Resume.pdf" class="w-300 h-[600px] m-auto rounded-2xl"></iframe>
           </div>
         </main>
     </div>
