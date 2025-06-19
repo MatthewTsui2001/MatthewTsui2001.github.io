ec5e0eee68287b3773c9321c44d9195db4d6ec4b
MatthewTsui2001 <matttsui2001@gmail.com>
2025-06-19 11:34:20 -0700

    added the title for each individual page #2

--- app/Contact/layout.js
+++ app/Contact/layout.js
@@ -1,8 +1,8 @@
 import "../globals.css";
 
 export const metadata = {
-  title: "",
-  description: "",
+  title: "Matthew Tsui - Contact",
+  description: "The contact page for the portfolio website",
 };
 
 export default function RootLayout({ children }) {
