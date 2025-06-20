76b61f050cd266d21143cd80d74dcbae4acc2de7
MatthewTsui2001 <matttsui2001@gmail.com>
2025-06-19 17:44:28 -0700

    added my instagram #35

--- app/Contact/page.js
+++ app/Contact/page.js
@@ -15,10 +15,11 @@ export default function Home() {
         </header>
        <main class="flex flex-col  my-5 mx-auto bg-gradient-to-t from-gray-500 to-white">
             <h1 class="text-center text-5xl sm:text-6xl md:text-7xl my-15">Contact Me:</h1>
-            <div class="bg-gray-200 mx-auto my-15 border-2 border-green-500  px-4 py-2 grid grid-cols-3">
+            <div class="bg-gray-200 mx-auto my-15 border-2 border-green-500  px-4 py-2 grid grid-cols-4">
                 <a href="mailto:matttsui2001@gmail.com" class="hover:opacity-40 hover:scale-3d"><img src="gmail.svg" alt="Gmail logo" class="w-25 h-25"/></a>
                 <a href="https://www.linkedin.com/in/matthewtsui123/" class="hover:opacity-40 hover:scale-3d"><img src="linkedin.svg" alt="Linkedin logo" class="w-25 h-25"/></a>
                 <a href="https://github.com/MatthewTsui2001" class="hover:opacity-40 hover:scale-3d"><img src="github.svg" alt="github logo" class="w-25 h-25"/></a>
+                <a href="https://www.instagram.com/matttsui_/" class="hover:opacity-40 hover:scale-3d"><img src="instgram.svg" alt="The Instagram Logo" class="w-25 h-25"></img></a>
             </div>
         </main>
     </div>
